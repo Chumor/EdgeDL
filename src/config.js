f4fb6a766fcd16166b00adb71d49abdd3ec1e861
Chumor <185651145+Chumor@users.noreply.github.com>
2026-02-23 23:27:04 +0800

    refactor: 重构默认下载器配置与存储逻辑

--- src/config.js
+++ src/config.js
@@ -7,18 +7,25 @@ export const DOWNLOADERS = {
 };
 
 // 默认下载器
-export let selectedDownloader = GM_getValue('edgedl-default-downloader') || DOWNLOADERS.IDM;
+export const DEFAULT_DOWNLOADER_KEY = 'edgedl-default-downloader';
+export const DEFAULT_DOWNLOADER = DOWNLOADERS.IDM;
+
+// 当前选中的下载器
+export let selectedDownloader = GM_getValue(
+    DEFAULT_DOWNLOADER_KEY,
+    DEFAULT_DOWNLOADER
+);
 
 // 设置默认下载器
 export function setDefaultDownloader(pkg) {
     selectedDownloader = pkg;
-    GM_setValue('edgedl-default-downloader', pkg);
+    GM_setValue(DEFAULT_DOWNLOADER_KEY, pkg);
 }
 
 // 重置默认下载器
 export function resetDefaultDownloader() {
-    selectedDownloader = DOWNLOADERS.IDM;
-    GM_deleteValue('edgedl-default-downloader');
+    selectedDownloader = DEFAULT_DOWNLOADER;
+    GM_deleteValue(DEFAULT_DOWNLOADER_KEY);
 }
 
 // 下载链接关键字匹配
