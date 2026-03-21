c26c89c9b4eb0ebdb5649a82ff9c4af2df4ff6ae
Chumor <185651145+Chumor@users.noreply.github.com>
2026-03-21 14:53:43 +0800

    feat: 合并多个下载器模块为统一入口

--- src/adapter/abdm.js
+++
@@ -1,7 +0,0 @@
-import { DOWNLOADERS } from '../core/config.js'
-import { buildIntentUrl } from './utils.js';
-
-export function openABDM(url, packageName = DOWNLOADERS.ABDM) {
-  const intentUrl = buildIntentUrl(url, packageName);
-  window.location.href = intentUrl
-}
